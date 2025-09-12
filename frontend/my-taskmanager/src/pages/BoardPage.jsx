d787a0f757a61ce19a5ea71e9d3788cc9cd92532
Petr <petr.samusev01@gmail.com>
2025-09-12 16:49:57 +0700

    Add AddColumnForm

--- frontend/my-taskmanager/src/pages/BoardPage.jsx
+++ frontend/my-taskmanager/src/pages/BoardPage.jsx
@@ -1,15 +1,15 @@
 import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { addColumn, getColumns, getTasks, getToken } from "../api";
+import { getColumns, getTasks, getToken } from "../api";
 import AddTaskForm from "./pageElements/AddTaskForm";
 import Task from "./pageElements/Task";
+import AddColumnForm from "./pageElements/AddColumnForm";
 
 export default function BoardPage() {
   const { id } = useParams();
   const [columns, setColumns] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
-  const [newColumnName, setNewColumnName] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -51,43 +51,16 @@ export default function BoardPage() {
     loadBoardData();
   }, [id, navigate]);
 
+  const handleColumnAdded = (newColumn) => {
+    setColumns((prevColumns) => [...prevColumns, newColumn]);
+  };
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p style={{ color: "red" }}>{error}</p>;
 
-  async function handleSubmitColumn(e) {
-    e.preventDefault();
-
-    if (!newColumnName.trim()) {
-      setError("Column name can not be empty");
-      return;
-    }
-    
-    try {
-      const result = await addColumn(id, newColumnName);
-      setColumns([...columns, result]);
-      setNewColumnName("");
-      setError("");
-    } catch (err) {
-      setError("Failed to add new Column");
-      console.error(err);
-    }
-  }
-
   return (
     <div style={{ padding: "20px" }}>
-      <div style={{ border: "1px dashed gray", padding: "10px" }}>
-        <form onSubmit={handleSubmitColumn}>
-          <input
-            type="text"
-            placeholder="New column name"
-            value={newColumnName}
-            onChange={(e) => setNewColumnName(e.target.value)}
-          />
-          <br />
-          <button type="submit">Add new column</button>
-          {error && <p style={{ color: "red" }}>{error}</p>}
-        </form>
-      </div>
+      <AddColumnForm boadId={id} onColumnAdded={handleColumnAdded} />
 
       <div style={{ display: "flex", gap: "20px" }}>
         {columns.map((column) => (
@@ -103,20 +76,7 @@ export default function BoardPage() {
             <h2>{column.title}</h2>
 
             {column.tasks?.map((task) => (
-              <Task key = {task.id} task = {task}/>
-              /*
-              <div
-                key={task.id}
-                style={{
-                  border: "1px solid gray",
-                  margin: "5px 0",
-                  padding: "5px",
-                }}
-              >
-                <strong>{task.title}</strong>
-                <p>{task.description}</p>
-              </div>
-              */
+              <Task key={task.id} task={task} />
             ))}
 
             <AddTaskForm
